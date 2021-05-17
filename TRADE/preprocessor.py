b71313969585cd71c367973dff00cc6158ae8c4c
dhh0 <76548813+dhh0@users.noreply.github.com>
2021-05-18 01:41:29 +0900

    Update preprocessor.py
    sep_custom_convert_examples_to_features 추가

--- TRADE/preprocessor.py
+++ TRADE/preprocessor.py
@@ -110,12 +110,61 @@ class TRADEPreprocessor(DSTPreprocessor):
         return OpenVocabDSTFeature(
             example.guid, input_id, segment_id, gating_id, target_ids
         )
+    
+    def _sep_custom_convert_example_to_feature(self, example):
+        context = "".join(example.context_turns) + " [SEP] "
+        current = "".join(example.current_turn)
+        context_id = self.src_tokenizer.encode(context, add_special_tokens=False)
+        current_id = self.src_tokenizer.encode(current, add_special_tokens=False)
 
+        context_segment_id = [0] * len(context_id)
+        current_segment_id = [1] * len(current_id)
+        segment_id = context_segment_id + current_segment_id
+        input_id = context_id + current_id
+        # max_seq_length를 넘어가면 좌측부터 truncate
+        max_length = self.max_seq_length - 2  # 나중에 붙여줄 처음과 끝의 cls_token_id, sep_token_id 고려
+        if len(input_id) > max_length:
+            gap = len(input_id) - max_length
+            input_id = input_id[gap:]
+            segment_id = segment_id[gap:]
+
+        # text로 생각하면 [CLS] + [SEP] + tokenized_texts + [SEP] + tokenized_texts + ... + [SEP]
+        input_id = (
+                [self.src_tokenizer.cls_token_id]
+                + input_id
+                + [self.src_tokenizer.sep_token_id]
+        )
+        segment_id = [0] + segment_id + [1]
+
+        target_ids = []
+        gating_id = []
+        if not example.label:
+            example.label = []
+
+        state = convert_state_dict(example.label)
+        for slot in self.slot_meta:
+            value = state.get(slot, "none")
+            target_id = self.trg_tokenizer.encode(value, add_special_tokens=False) + [
+                self.trg_tokenizer.sep_token_id
+            ]
+            target_ids.append(target_id)
+            gating_id.append(self.gating2id.get(value, self.gating2id["ptr"]))
+        target_ids = self.pad_ids(target_ids, self.trg_tokenizer.pad_token_id)
+        return OpenVocabDSTFeature(
+            example.guid, input_id, segment_id, gating_id, target_ids
+        )
+
+    
     def convert_examples_to_features(self, examples):
         return list(map(self._convert_example_to_feature, examples))
 
     def custom_convert_examples_to_features(self, examples):
         return list(map(self._custom_convert_example_to_feature, examples))
+    
+    def sep_custom_convert_examples_to_features(self, examples):
+    return list(map(self._sep_custom_convert_example_to_feature, examples))
+
+
 
     # inference된 결과물을 평가할 수 있는 format으로 만들어 주는 부분
     def recover_state(self, gate_list, gen_list):
